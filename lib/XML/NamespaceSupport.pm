1aaad487174b0dca38e052a6aa273c5c1765eb6b
Paul Cochrane <paul@liekut.de>
2015-01-03 13:44:32 +0100

    Updating the namespaces link in POD
    As mentioned in RT#61856 the link to the NamespaceSupport page is stale.
    The updated link points to the new official location.

--- lib/XML/NamespaceSupport.pm
+++ lib/XML/NamespaceSupport.pm
@@ -377,7 +377,7 @@ from within any application that may need them. It also helps maintain
 a prefix to namespace URI map, and provides a number of basic checks.
 
 The model for this module is SAX2's NamespaceSupport class, readable at
-http://www.megginson.com/SAX/Java/javadoc/org/xml/sax/helpers/NamespaceSupport.html.
+http://www.saxproject.org/namespaces.html
 It adds a few perlisations where we thought it appropriate.
 
 =head1 METHODS
